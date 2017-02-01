fdaf75cea78fdaa2bed1934583836eac849424f9
Nathanaël Spriet <nathanael.spriet@gmail.com>
2017-02-01 17:17:41 +0100

    Adds some comments

--- automaton.c
+++ automaton.c
@@ -14,18 +14,20 @@ void generateTransitionMatrix()
 	int rowIncrement = 0;
 	// Memory allocation for the first level of pointer
 	transitionMatrix = malloc(STATE_NUMBER*sizeof(InputState*));
-	// Browse the rows of the array
+	// Browse the rows of the matrix
 	for(rowIncrement = 0; rowIncrement < STATE_NUMBER; rowIncrement++)
 	{
 		// Memory allocation for the second level of pointers
 		transitionMatrix[rowIncrement] = malloc(INPUT_STATE_NUMBER*sizeof(InputState));
 		int colIncrement = 0;
-		// Browse the columns of the array
+		// Browse the columns of the matrix
 		for(colIncrement = 0; colIncrement < INPUT_STATE_NUMBER; colIncrement++)
 		{
+			// Assign default value
 			transitionMatrix[rowIncrement][colIncrement] = NOT_CORRECT;
 		}
 	}
+	// Assign specific values
 	transitionMatrix[0][0] = FOUND_FIRST_ARTICLE;
 	transitionMatrix[0][4] = FOUND_FIRST_PROPER_NOUN;
 	transitionMatrix[1][1] = FOUND_FIRST_ARTICLE;
@@ -64,21 +66,29 @@ int parseSentence(char* sentence)
 	OutputState finishedState = NOT_FINISHED;
 	const char delimiter[2] = " ";
 	int sentenceLength = strlen(sentence);
+	// Process the lack of space before the final period
 	if(sentence[sentenceLength-1] == '.' && sentence[sentenceLength-2] != ' ')
 	{
+		// Copy a space and a period at the end of sentence
 		strcpy(sentence+sentenceLength-1, " .");
 	}
 	char *foundWord;
+	// Found the first word
 	foundWord = strtok(sentence, delimiter);
 
 	// Browse the other words
 	while( foundWord != NULL  && finishedState == NOT_FINISHED)
 	{
+		// Find the type of the word in the dictionary
 		InputType wordType = findTypeOfWord(foundWord);
+		// Change the current state based on the transition matrix
 		currentState = transitionMatrix[currentState][wordType];
+		// Find the finished state based on the current state
 		finishedState = actionOfState(currentState);
+		// Found the next word
 		foundWord = strtok(NULL, delimiter);
 	}
+	// If the automaton has not processed to a final period
 	if(finishedState == NOT_FINISHED)
 	{
 		return(0);
@@ -89,8 +99,10 @@ int parseSentence(char* sentence)
 InputType findTypeOfWord(char* word)
 {
 	int i = 0;
+	// Browses the dictionary
 	for(i = 0; i < DICTIONARY_SIZE; i++)
 	{
+		// If the word is found return its type
 		if(!strcmp(dictionary[i].word,word))
 		{
 			return(dictionary[i].inputType);
